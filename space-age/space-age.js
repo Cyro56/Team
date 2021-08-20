12e30acfff785bed38f2e870c5e455b8bd7cef1b
cyro.costa <cyro.costa@ambevtech.com.br>
2021-08-20 15:35:59 -0300

    adcionando nova versão da solução

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -2,47 +2,42 @@
 const anosDaTerraEmSegundos = 31557600;
 
 export const age = (planeta,segundosTotais) => {
+ var factor;
 if(planeta == "earth"){
-  var factor = 1.0
-  var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos terrestres
-  return parseFloat(anosPlaneta.toPrecision(4)); //retornando os anos terrestres
+  factor = 1.0;
+  
 }
 
 if(planeta == "mercury"){
-  var factor = 0.2408467
-  var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos em mercúrio
-  return parseFloat(anosPlaneta.toPrecision(5)); //retornando os anos em mercúrio
+  factor = 0.2408467;
+ 
 }
 if(planeta == "venus"){
-  var factor = 0.61519726
-  var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos em vênus
-  return parseFloat(anosPlaneta.toPrecision(3)); //retornando os anos em vênus
+  factor = 0.61519726;
+
 }
 if(planeta == "mars"){
-  var factor = 1.8808158
-  var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos em marte
-  return parseFloat(anosPlaneta.toPrecision(4)); //retornando os anos em marte
+ factor = 1.8808158;
+
 }
 if(planeta == "jupiter"){
-  var factor = 11.862615
-  var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos em júpiter
-  return parseFloat(anosPlaneta.toPrecision(3)); //retornando os anos em júpiter
+ factor = 11.862615;
+
 }
 if(planeta == "saturn"){
-  var factor = 29.447498
-  var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos em saturno
-  return parseFloat(anosPlaneta.toPrecision(3)); //retornando os anos em saturno
+ factor = 29.447498;
+
 }
 if(planeta == "uranus"){
-  var factor = 84.016846
-  var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos em urano
-  return +(anosPlaneta.toPrecision(2)); //retornando os anos em urano
+ factor = 84.016846;
+ 
 }
 if(planeta == "neptune"){
-  var factor = 164.79132
-  var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos em netuno
-  return +(anosPlaneta.toPrecision(2)); //retornando os anos em netuno
+ factor = 164.79132;
+ 
 }
+var anosPlaneta = (segundosTotais/(anosDaTerraEmSegundos))/factor; // Indicando quantos anos terrestres
+  return parseFloat(anosPlaneta.toFixed(2)); //retornando os anos terrestres
 
 };
 
